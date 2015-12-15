af23edd827b5a332ab80337d2e39638a767e70b3
Jon Billett <jon@getsidecar.com>
2015-12-15 16:34:40 -0500

    2 bug fixes and 1 enhancement complete

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,4 +1,3 @@
-// Initial todos. For now, hard code this, should get this state from persistent storage later.
 var todos = [
   {
     id: 1,
@@ -12,22 +11,31 @@ var todos = [
   }
 ];
 
-// Increment global ID so they are unique.
 var id = todos.length;
+
 function incrementId() {
   id++;
   return id;
 }
 
+function renderTotals() {
+  $('stats').html('')
+}
+
 function renderTodos() {
-
   $('.todos').html('');
-
-  for (var i = 0; i < todos.length; i++) {
-    var todo = todos[i];
-    var checked = todo.complete ? 'checked' : ''
-    $('.todos').append("<li class='todo' data-id=" + todo.id + "><label><input class='toggle-todo' type='checkbox' " + checked + "/> " + todo.text + "</label></li>");
-  }
+    for (var i = 0; i < todos.length; i++) {
+      var todo = todos[i];
+      //Had to figure out what the hell this is. This is called a ternery - it's an alternate way of writing an if/else statement
+      var checked = todo.complete ? 'checked' : '';
+      //----------------NEW FEATURE---------------\\
+      //Create a 2nd ternery that checks completion. If it is complete, add a .css style that iwll give it a line-through
+      var listClass = todo.complete ? 'todo completed' : 'todo';
+      $('.todos').append("<li class='" + listClass + "' data-id=" + todo.id + "><label><input class='toggle-todo' type='checkbox' " + checked + "/> " + todo.text + "</label></li>");
+    }
+    //----------------BUG FIX---------------\\
+    //after the new to do is rendered, clear the value of the .todo-text form field
+    $('.todo-text').val("");
 }
 
 function findById(id) {
@@ -43,34 +51,37 @@ function findById(id) {
 }
 
 $(document).ready(function() {
-  // Initialize with any existing todos.
   renderTodos();
 
-  // Bind to input update to mark todo as complete.
   $(document).on('change', '.toggle-todo', function(event) {
     var id = $(event.target).parent().parent().data('id');
     var todo = findById(id);
 
     todo.complete = event.target.checked;
-
     renderTodos();
   });
 
-  // Bind to new todo form submission to create new todos.
+
   $(document).on('submit', '.new-todo', function(event) {
     event.preventDefault();
 
     var text = $('.todo-text').val();
 
-    var newTodo = {
-      id: incrementId(),
-      text: text,
-      completed: false
-    };
-
-    todos.push(newTodo);
-
-    renderTodos();
+    //----------------BUG FIX---------------\\
+    //insert an if/else to check the length of the text variable (the value of .todo-text)
+    if (text.length > 0) {
+        var newTodo = {
+          id: incrementId(),
+          text: text,
+          completed: false
+          };
+        todos.push(newTodo);
+        renderTodos();
+      }
+      else{
+        alert("What do ya say you quit playing games and actually put some text in the box?");
+      }
+ 
   });
 
 });
